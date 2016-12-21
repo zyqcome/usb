8149fa5d293e4c50b2b82964ecd8d13a20790e69
Zyqtest <zyqcome@126.com>
2016-12-21 15:55:54 +0800

    init-0

---
+++ TinyShop/TinyShop/Class/Other/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  TinyShop
+//
+//  Created by rimi on 16/12/21.
+//  Copyright © 2016年 cc.zyqblog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
---
+++ TinyShop/TinyShop/Class/Other/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TinyShop
+//
+//  Created by rimi on 16/12/21.
+//  Copyright © 2016年 cc.zyqblog. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
